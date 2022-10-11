60ad381c8e8e599c6e18524866c10879f9509a5b
Alekseikovtun <aleksei.kovtun.earth@gmail.com>
2022-10-11 14:59:36 +0300

    select_2

--- main.py
+++ main.py
@@ -8,10 +8,48 @@ connection = engine.connect()
 # print(engine.get_table_names())
 # """What tables are in the database"""
 
-# name and year of release of albums released in 2018
-select_1 = connection.execute(
-    """SELECT albumname, releasealbumdate FROM album 
-    WHERE releasealbumdate BETWEEN '2018-01-01' AND '2018-12-31';"""
+# # Number of singers in each style
+# select_1 = connection.execute(
+#     """;"""
+# )
+# result_1 = select_1.all()
+# print(result_1, '\n')
+
+# Number of songs included in albums 2019-2020
+select_2 = connection.execute(
+    """SELECT album_name, release_album_date, num_of_songs 
+    FROM album AS a
+    LEFT JOIN song AS s ON s.album_id = id_album 
+    WHERE (release_album_date>='2019-01-01') AND (release_album_date<='2020-12-31')
+    GROUP BY (album_name, release_album_date, num_of_songs);"""
 )
-result_1 = select_1.all()
-print(result_1, '\n')
+result_2 = select_2.all()[0][2]
+print(result_2, '\n')
+
+# # Number of singers in each style
+# select_1 = connection.execute(
+#     """;"""
+# )
+# result_1 = select_1.all()
+# print(result_1, '\n')
+
+# # Number of singers in each style
+# select_1 = connection.execute(
+#     """;"""
+# )
+# result_1 = select_1.all()
+# print(result_1, '\n')
+
+# # Number of singers in each style
+# select_1 = connection.execute(
+#     """;"""
+# )
+# result_1 = select_1.all()
+# print(result_1, '\n')
+
+# # Number of singers in each style
+# select_1 = connection.execute(
+#     """;"""
+# )
+# result_1 = select_1.all()
+# print(result_1, '\n')
